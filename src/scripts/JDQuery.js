52aa031c5669b92f3a6529f289e2d5dc65da67a8
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-22 15:10:47 +0700

    adding JDQuery class and small functionality

--- src/scripts/JDQuery.js
+++ src/scripts/JDQuery.js
@@ -1,1 +1,24 @@
-// JDQuery
+class JDQuery {
+  constructor(query = null) {
+    this._document = document.querySelector(query);
+    this.len = this._document.length;
+  }
+
+  html(replacer = null) {
+    if (!replacer) {
+      return this._document.innerHTML;
+    }
+
+    this._document.innerHTML = replacer;
+  }
+
+  text(replacer = null) {
+    if (!replacer) {
+      return this._document.innerText;
+    }
+
+    this._document.innerText = replacer;
+  }
+}
+
+export default JDQuery;
