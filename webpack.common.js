dd04c226545af94f9bb329079c288c841a5a3808
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-22 13:59:05 +0700

    JDQuery Refactoring

---
+++ webpack.common.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jdquery-bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+};
